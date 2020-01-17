c60a7c2452a7d113aab87bf9b9aadf177e3dcc10
mhieupham1 <mhieupham1@gmail.com>
2020-01-17 16:04:37 +0700

    Challenge 8

--- index.php
+++ index.php
@@ -55,6 +55,6 @@ $array = [
 //Sử dụng git để clone tập tin này
 //In ra màn hình dưới dạng html tên thương hiệu và tên sản phẩm
 //Tên thương hiệu phải được in ra bằng tiếng việt ví dụ 'tenthuonghieu'=>'Samsung Phone', in ra html = Điện thoại Samsung
-//Nếu name = 'Apple Phone' và name = 'Apple Headphone' thì gộp product vào và chung thương hiệu ghi là Apple
+//Nếu tenthuonghieu = 'Apple Phone' và tenthuonghieu = 'Apple Headphone' thì gộp sanpham của cả hai vào một và chung tên thương hiệu ghi là Apple
 //Khi làm xong sử dụng git tạo một branch mới và push code lên branch ấy
 //Sau khi push code lên , tạo một pull request giữa branch vừa tạo và branch master
